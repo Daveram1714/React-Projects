8503f5835d9f39186a369e8cca849be654c0c107
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-09-07 07:11:31 +0530

    #

--- src/Content.js
+++ src/Content.js
@@ -1,24 +1,52 @@
 import React from 'react'
 import { useState } from 'react';
+import { FaTrashCan } from "react-icons/fa6";
 
 
 const Content = () => {
-
-     const[name,setName] = useState("Increase");
-        function HandelNamechange(){
-          const name = ["Earn","Grow","Give"];
-          const int = Math.floor(Math.random()*3);
-          setName (name[int])
-     }
-
+   const [items,Setitems] = useState(
+     [
+          {
+               id:1,
+               checked:true,
+               item:"Learningreact"
+          },
+          {
+               id:1,
+               checked:true,
+               item:"Learning MongoDb"
+          },
+          {
+               id:1,
+               checked:true,
+               item:"Placements"
+          },
+          {
+               id:1,
+               checked:true,
+               item:"Learning  Subjects"
+          }
+     ]
+   );
 
 
   return ( 
 
    <main>
-        <p>  Let's {name} Money</p>
-        <button onClick={HandelNamechange}> Click Me </button>
-        
+      <ul>
+          {items.map((item) => 
+          <li>
+         <input  
+          type='checkbox'
+          checked ={item.checked}
+          />
+               <label>{item.item}</label>
+               <><FaTrashCan />
+               </>
+          </li>
+  
+          )}
+      </ul>
    </main>     
 )
 }
