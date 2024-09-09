b559478d8fdaebccb7127cdb0f578654b24d6161
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-09-09 07:00:06 +0530

    #

--- src/Content.js
+++ src/Content.js
@@ -1,53 +1,12 @@
 import React from 'react'
-import { useState } from 'react';
 import { FaTrashCan } from "react-icons/fa6";
 
 
-<<<<<<< Updated upstream
-const Content = () => {
-   const [items,Setitems] = useState(
-     [
-          {
-               id:1,
-               checked:true,
-               item:"Learningreact"
-          },
-          {
-               id:2,
-               checked:false,
-               item:"Learning MongoDb"
-          },
-          {
-               id:3,
-               checked:false,
-               item:"Placements"
-          },
-          {
-               id:4,
-               checked:false,
-               item:"Learning  Subjects"
-          }
-     ]
-   );
-
-   const Handelcheck = (id) => {
-          const listitems = items.map((item) => 
-          item.id ===id? {...item,checked : !item.checked}: item
-          )     
-          Setitems(listitems)
-
-   }
-=======
 const Content = ({items,HandelCheck,HandelDelete}) => {
->>>>>>> Stashed changes
 
-   const Handeldelete =  (id) =>{
-     const listitems = items.filter((item) =>
-          item.id !==id)
-          Setitems(listitems)
-   }
 
-  return ( 
+  
+ return ( 
 
    <main>
       <ul>
@@ -55,14 +14,14 @@ const Content = ({items,HandelCheck,HandelDelete}) => {
           <li className="item" key={item.id}>
          <input  
           type='checkbox'
-          onChange={() => Handelcheck(item.id) }
+          onChange={() => HandelCheck(item.id) }
           checked ={item.checked}
           />
                <label>{item.item}</label>
                <FaTrashCan 
                role='button'
                tabIndex={0}
-                onClick={() => Handeldelete(item.id)}
+                onClick={() => HandelDelete(item.id)}
                />
                
           </li>
@@ -72,5 +31,6 @@ const Content = ({items,HandelCheck,HandelDelete}) => {
    </main>     
 )
 }
+
 
 export default Content
