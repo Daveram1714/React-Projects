0aa76a7f6107cf09bdb309e1de4a612d1b135c31
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-09-09 06:35:46 +0530

    #

--- src/Content.js
+++ src/Content.js
@@ -1,62 +1,80 @@
 import React from 'react'
-import { useState } from 'react';
 import { FaTrashCan } from "react-icons/fa6";
 
 
-const Content = () => {
-   const [items,Setitems] = useState(
-     [
-          {
-               id:1,
-               checked:false,
-               item:"Learningreact"
-          },
-          {
-               id:2,
-               checked:false,
-               item:"Learning MongoDb"
-          },
-          {
-               id:3,
-               checked:false,
-               item:"Placements"
-          },
-          {
-               id:4,
-               checked:false,
-               item:"Learning  Subjects"
-          }
-     ]
-   );
+const Content = (items,HandelCheck,HandelDelete) => {
+//    const [items,Setitems] = useState(
+//      [
+//           {
+//                id:1,
+//                checked:false,
+//                item:"Learningreact"
+//           },
+//           {
+//                id:2,
+//                checked:false,
+//                item:"Learning MongoDb"
+//           },
+//           {
+//                id:3,
+//                checked:false,
+//                item:"Placements"
+//           },
+//           {
+//                id:4,
+//                checked:false,
+//                item:"Learning  Subjects"
+//           }
+//      ]
+//    );
 
-   const Handelcheck = (id) =>{
-     const listitems = items.map((item) =>
-     item.id===id ? {...item , checked : !item.checked} :item)
-     Setitems(listitems)
-   }
+//    const Handelcheck = (id) =>{
+//      const listitems = items.map((item) =>
+//      item.id===id ? {...item , checked : !item.checked} :item)
+//      Setitems(listitems)
+//      localStorage.setItem("todo-list",JSON.stringify(listitems))
+//    }
+
+//    const Handeldelete = (id) => {
+
+//      const listitems  = items.filter((item) =>
+//      item.id !== id )
+//      Setitems(listitems)
+//      localStorage.setItem("todo-list",JSON.stringify(listitems))
+//    }
+
 
 
   return ( 
 
    <main>
+     {(items.length) ? (
       <ul>
           {items.map((item) => 
           <li className="item" key={item.id}>
          <input  
           type='checkbox'
-          onChange={() => Handelcheck(item.id) }
+          onChange={() => HandelCheck(item.id) }
           checked ={item.checked}
           />
-               <label>{item.item}</label>
+               <label 
+               style={(item.checked) ? {textDecoration : 'line-through'}: null}
+               
+               onClick={() => HandelCheck(item.id)}
+               >{item.item}</label>
+
                <><FaTrashCan 
+               onClick={()=> HandelDelete(item.id)}
                role='button'
                tabIndex={0}
                />
                </>
           </li>
-  
           )}
       </ul>
+     ) :
+     ( <p> Your List is EMpty</p>)
+}
    </main>     
 )
 }
