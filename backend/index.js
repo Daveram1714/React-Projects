43b74f1bf2cb58eb3117f4d9b46ae29b6720d940
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-09-15 06:20:42 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,31 @@
-import express from "express";
+import express from 'express';
+import connectDB from "./db/db.js";
+import userRoutes from "./route/todo.route.js";
 
-const app  = express();
-const PORT = 6969;
+const app = express();
+const PORT = 6969
 
-app.get("/" , (req,res) => {
-    res.json({msg: "Hello"})
-});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/bike").then(()=>{
+//     console.log("DB connected successfully")
+// })
+// .catch(()=>{
+//     console.log("DB connection failed")
+// })
 
-       
+connectDB();
+
+app.use(express.json())
+// app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/user",userRoutes)
+
+app.get("/",(res,req)=>{
+    res.json({msg:"Hello !."})
+} )
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running in the Port http://localhost:${PORT}`);
+    
+})
